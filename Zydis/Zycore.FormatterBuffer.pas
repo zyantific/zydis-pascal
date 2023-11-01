95cb837147e887c621aea6c82e9830dc81ae56f5
Coldzer0 <6647006+Coldzer0@users.noreply.github.com>
2023-11-01 17:23:31 +0200

    Add License to units headers

--- Zydis/Zycore.FormatterBuffer.pas
+++ Zydis/Zycore.FormatterBuffer.pas
@@ -1,3 +1,26 @@
+{*******************************************************************************
+
+  Zydis Pascal API By Coldzer0
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+
+*******************************************************************************}
 unit Zycore.FormatterBuffer;
 
 {$IFDEF FPC}
@@ -12,7 +35,8 @@ uses
   Zydis.Types,
   Zycore.Strings;
 
-type
+
+Type
   { Defines the ZydisFormatterBuffer struct. }
   TZydisFormatterBuffer = record
     { ZYAN_TRUE, if the buffer contains a token stream or ZYAN_FALSE, if it contains a simple string. }
